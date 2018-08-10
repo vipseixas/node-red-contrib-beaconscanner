1c97f222423e44ceef3b81b247947e5664318fff
Vinicius Seixas <vinicius@seixasonline.net>
2018-08-10 02:04:24 -0300

    Minor changes

--- BeaconScanner.js
+++ BeaconScanner.js
@@ -4,56 +4,63 @@ module.exports = function (RED) {
 
 	let setStatus = (node, scanning, error) => {
 		node.status({
-			fill: scanning ? "blue" : error ? "red" : "gray",
-			shape: "dot",
+			fill: scanning ? "blue" : error ? "red" : "grey",
+			shape: scanning ? "dot" : "ring",
 			text: scanning ? "scanning" : error ? "error" : "stopped"
 		});
+
+		node.scanning = scanning;
 	};
 
 	let start = (node, scanner) => {
+		if (node.scanning) return;
+
 		scanner.startScan().then(() => {
 			setStatus(node, true)
-			console.log('Scanner started');
+			node.log('Scanner started');
 		}).catch((error) => {
 			setStatus(node, false, error);
-			console.error('Error starting scanner', JSON.stringify(error));
+			node.error('Error starting scanner', { payload: {}, error: error });
 		});
 	};
 
 	let stop = (node, scanner) => {
+		if (!node.scanning) return;
+
 		scanner.stopScan();
 		setStatus(node, false)
-		console.log('Scanner stopped');
+		node.log('Scanner stopped');
 	}
 
-	function BeaconScanner(n) {
-		RED.nodes.createNode(this, n);
+	function BeaconScanner(config) {
+		RED.nodes.createNode(this, config);
+
+		let node = this;
 
 		// Initial status
-		setStatus(this, false);
+		setStatus(node, false);
 
 		// Read input
-		this.on("input", function (msg) {
-			if (msg.payload === true || msg.payload === "on") {
-				start(this, scanner);
+		node.on("input", function (msg) {
+			if (msg.payload === true || msg.payload === 1 || msg.payload === "on") {
+				start(node, scanner);
 			} else {
-				stop(this, scanner);
+				stop(node, scanner);
 			}
 		});
 
 		// Send message when receive an advertisement
 		scanner.onadvertisement = (data) => {
 			msg = {};
-			msg.topic = this.topic;
+			msg.topic = node.topic;
 			msg.payload = JSON.stringify(data);
-			this.send(msg);
-
-			console.log('Sending message payload', msg.payload);
+			node.send(msg);
+			node.trace('Sending message payload', msg.payload);
 		};
 
 		// Stop scanning on close
-		this.on("close", function () {
-			stop(this, scanner);
+		node.on("close", function () {
+			stop(node, scanner);
 		});
 	}
 
